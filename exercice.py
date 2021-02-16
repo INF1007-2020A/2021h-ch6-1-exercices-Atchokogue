7f37edb119f34c0641d9200859ea834a8d5c13e6
Atchokogue <77995797+Atchokogue@users.noreply.github.com>
2021-02-16 14:14:58 -0500

    Update exercice.py

--- exercice.py
+++ exercice.py
@@ -24,39 +24,58 @@ def anagrams(words: list = None) -> bool:
 
 
 def contains_doubles(items: list) -> bool:
-    return False
+    return len(set(items)) != len(items)
 
 
 def best_grades(student_grades: dict) -> dict:
     # TODO: Retourner un dictionnaire contenant le nom de l'étudiant ayant la meilleure moyenne ainsi que sa moyenne
-    return {}
+    best_student = dict()
+    for key, value in student_grades.items():
+        average = sum(value) / len(value)
+
+        if len(best_student) == 0 or list(best_student.values())[0] < average:
+            best_student = {key: average}
+    return best_student
 
 
 def frequence(sentence: str) -> dict:
     # TODO: Afficher les lettres les plus fréquentes
     #       Retourner le tableau de lettres
+    frequency = {letter: sentence.count(letter) for letter in sentence}
+    sorted_keys = sorted(frequency, reverse=True, key=frequency.__getitem__)
+    for key in sorted_keys:
+        if frequency[key] > 5:
+            print(f"Le caractere {key} revient {frequency[key]} fois.")
 
-    return {}
+    return frequency
 
 
 def get_recipes():
     # TODO: Demander le nom d'une recette, puis ses ingredients et enregistrer dans une structure de données
-    pass
+    name = input("quel est le nom de cette recette?\n")
+    ingredient = input("Entrer la liste d'ingédients, séparer les ingrédients par une ','\n")
+
+    return {name: ingredient}
 
 
 def print_recipe(ingredients) -> None:
     # TODO: Demander le nom d'une recette, puis l'afficher si elle existe
-    pass
+    name = input("")
+
+    if name in ingredients:
+        print(ingredients[name])
+    else: print("Recette non-existante")
+
 
 
 def main() -> None:
-    """print(f"On essaie d'ordonner les valeurs...")
-    print(order())"""
+    print(f"On essaie d'ordonner les valeurs...")
+    print(order())
 
     print(f"On vérifie les anagrammes...")
     print(anagrams())
 
-    """my_list = [3, 3, 5, 6, 1, 1]
+    my_list = [3, 3, 5, 6, 1, 1]
     print(f"Ma liste contient-elle des doublons? {contains_doubles(my_list)}")
 
     grades = {"Bob": [90, 65, 20], "Alice": [85, 75, 83]}
@@ -70,7 +89,7 @@ def main() -> None:
     recipes = get_recipes()
 
     print("On affiche une recette au choix...")
-    print_recipe(recipes)"""
+    print_recipe(recipes)
 
 
 if __name__ == '__main__':
